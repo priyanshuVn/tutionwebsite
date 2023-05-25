9c36c5d04b71992acadb77d2179de4517e085a80
priyanshuVn <134581631+priyanshuVn@users.noreply.github.com>
2023-05-25 12:05:14 +0530

    serve files

---
+++ static/index.js
@@ -0,0 +1,14 @@
+function func1(){
+
+    let emt = document.getElementById('dropbox');
+    emt.style.display='block';
+    
+
+}
+function func2(){
+
+    let emt = document.getElementById('dropbox');
+    emt.style.display='none';
+    
+
+}
